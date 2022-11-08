4b3e4022f55f111ffb45cc0d7dec0bacd548fd7b
Meg <marguerite.tong@gmail.com>
2022-11-08 12:04:55 +0000

    transformer_replication: implement multihead masked attention

--- transformer_replication.py
+++ transformer_replication.py
@@ -3,6 +3,7 @@ import transformers
 import torch as t
 import torch.nn as nn
 import utils_w1d1
+from typing import Union, List
 from fancy_einsum import einsum
 # %%
 tokenizer = transformers.AutoTokenizer.from_pretrained("gpt2")
@@ -31,4 +32,39 @@ class Embedding(nn.Module):
         return f"{self.num_embeddings}, {self.embedding_dim}"
 
 utils_w1d1.test_embedding(Embedding)
+# %%
+#TODO positional encoding
+# %%
+class LayerNorm(nn.Module):
+
+    def __init__(self, normalized_shape: Union[int, List[int]], eps: float = 1e-05, elementwise_affine: bool = True):
+        super().__init__()
+        self.normalized_shape = normalized_shape
+        self.eps = eps
+        self.elementwise_affine = elementwise_affine
+
+        if self.elementwise_affine:
+            self.weight = nn.Parameter(t.ones(self.normalized_shape))
+            self.bias = nn.Parameter(t.zeros(self.normalized_shape))
+
+    def forward(self, x: t.Tensor) -> t.Tensor:
+        normalized_shape_dims = 1 if isinstance(self.normalized_shape, int) else len(self.normalized_shape)
+        x_mean = x.mean(dim=list(range(x.dim()))[-normalized_shape_dims:], keepdim=True)
+        x_var = x.var(dim=list(range(x.dim()))[-normalized_shape_dims:], keepdim=True)
+        print(x.var())
+        x_scaled = (x - x_mean) / t.sqrt(x_var + self.eps)
+        print(x_scaled.var())
+        if self.elementwise_affine:
+            return x_scaled * self.weight + self.bias
+        print(self.bias.mean())
+        return x_scaled
+
+    def extra_repr(self) -> str:
+        pass
+
+utils_w1d1.test_layernorm_mean_1d(LayerNorm)
+utils_w1d1.test_layernorm_mean_2d(LayerNorm)
+utils_w1d1.test_layernorm_std(LayerNorm)
+utils_w1d1.test_layernorm_exact(LayerNorm)
+utils_w1d1.test_layernorm_backward(LayerNorm)
 # %%
